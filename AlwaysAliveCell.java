a31e65f583c2269d7e65a6e6773089173fd6b8c4
YaLian2003 <54329751+YaLian2003@users.noreply.github.com>
2022-05-15 13:39:54 -0700

    Create AlwaysAliveCell.java

---
+++ AlwaysAliveCell.java
@@ -0,0 +1,23 @@
+//Yangyang Lian
+public class AlwaysAliveCell extends AbstractCell {
+
+   public AlwaysAliveCell(int r, int c, ConwayWorld w) {
+       super(r, c, w);
+   }
+
+   public AbstractCell cellForNextGeneration() {
+       return this;
+   }
+
+   public boolean willBeAliveInNextGeneration() {
+       return true;
+   }
+
+   public boolean getIsAlive() {
+       return true;
+   }
+
+   public char displayCharacter() {
+       return '+';
+   }
+}
